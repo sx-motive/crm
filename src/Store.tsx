d22d99804c6ef67b2bb7382229122e205787e505
auswuchs <auswuchs@protonmail.com>
2023-01-10 13:39:23 +0300

    Clear state error

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,6 +1,9 @@
 import create from 'zustand';
 
-const useStore = create((set) => ({
-  count: 1,
-  inc: () => set((state) => ({ count: state.count + 1 })),
+interface StoreState {
+
+}
+
+const useStore = create<StoreState>((set) => ({
+
 }));
